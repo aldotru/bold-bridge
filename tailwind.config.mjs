201dd57419b1d99de147b3339e429917300ce2fe
Aldo Trujillo <aldo.trujillo96@gmail.com>
2024-03-28 16:15:14 -0600

    Install tailwindCSS with base styling (colors and fonts)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,13 +3,37 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		colors: {
-			"primary": "#26035D",
-			"secondary": {
+			primary: "#26035D",
+			secondary: {
 				300: "#7747FF",
 				700: "#6028FF",
 			},
-			"tertiary": "#FFD3B7",
+			tertiary: "#FFD3B7",
 			white: "#FAF7F0",
+		},
+		fontSize: {
+			// 18px
+			base: "1.125rem",
+			// 12px
+			xs: ["0.875rem", "1.4"],
+			// 16px
+			body: ["1rem", "1.5"],
+			h1: ["10rem", {
+				lineHeight: "1",
+				fontWeight: "700",
+			}],
+			h2: ["3.75rem", {
+				lineHeight: "1.2",
+				fontWeight: "500",
+			}],
+			h3: ["3rem", {
+				lineHeight: "1.3",
+				fontWeight: "500",
+			}],
+			h4: ["2rem", {
+				lineHeight: "1",
+				fontWeight: "500",
+			}]
 		},
 		extend: {},
 	},
